9519a67a9c49348cdb4ec14af051ada9bc43ec3a
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-02 16:45:46 +0100

    creazione griglie

--- js/script.js
+++ js/script.js
@@ -9,18 +9,61 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro (
 
 */
 
-// 1. creo le 3 griglie 
-    // 1.1 creo una funzione generale che crei un elemento e aggiunga una classe 
-    // 1.2 creo griglia 10 x 10 => for
-    // 1.3 creo griglia 9 x 9 => for
-    // 1.4 creo griglia 7 x 7 => for
 
-// 2. do un valore alla variabile => level = 1
+// 1. do un valore alla variabile => level = 1
 
-// 3. chiedo all'utente di scegliere il livello di difficoltà : 1, 2 o 3 => let levelChoice = prompt ()
-    // 3.1 confronto la scelta dell'utente con la variabile => if( levelChoice = 1)
-    // 3.2 se l'utente sceglie livello 1 , genero una griglia di numeri con range 1-100 
-    // 3.3 se l'utente sceglie livello 2 , genero una griglia di numeri con range 1-81 
-    // 3.4 se l'utente sceglie livello 3 , genero una griglia di numeri con range 1-49   
+// 2. chiedo all'utente di scegliere il livello di difficoltà : 1, 2 o 3 => let levelChoice = prompt ()
+    // 2.1 confronto la scelta dell'utente con la variabile => if( levelChoice = 1)
+    // 2.2 se l'utente sceglie livello 1 , genero una griglia di numeri con range 1-100/ griglia 10 x 10 => for
+    // 2.3 se l'utente sceglie livello 2 , genero una griglia di numeri con range 1-81/ griglia 9 x 9 => for
+    // 2.4 se l'utente sceglie livello 3 , genero una griglia di numeri con range 1-49/ griglia 7 x 7 => for
 
-// 4. creo funzione di callback per cambiare il colore della cella cliccata 
+// 3. creo funzione di callback per cambiare il colore della cella cliccata 
+
+
+const gridContainer = document.getElementById("grid");
+
+let level = 1;
+
+
+// chiedo all'utente di scegliere il livello di difficoltà : 1, 2 o 3 => let levelChoice = prompt ()
+let levelChoice = prompt ("Da quale livello vuoi partire: 1, 2 o 3 ?");
+console.log(levelChoice);
+
+
+if(levelChoice = level) {
+    // griglia 10 x 10
+    for (let i = 1; i <= 100; i++) {
+
+        let node = document.createElement("div");
+        node.classList.add("square", "square-l1");
+        gridContainer.append(node);
+
+    }
+}else if(levelChoice = 2) {
+    // griglia 9 x 9 
+    for (let i = 1; i <= 81; i++) {
+
+        let node = document.createElement("div");
+        node.classList.add("square", "square-l2");
+        gridContainer.append(node);
+    }
+
+} else if(levelChoice = 3) {
+    // griglia 7 x 7 
+    for (let i = 1; i <= 49; i++) {
+
+        let node = document.createElement("div");
+        node.classList.add("square", "square-l3");
+        gridContainer.append(node);
+
+    } 
+}
+
+
+
+
+
+
+
+
