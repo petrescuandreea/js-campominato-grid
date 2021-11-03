d0601d158b72336678220a023a757431aab3bb76
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-03 11:00:11 +0100

    refactoring

--- js/script.js
+++ js/script.js
@@ -11,48 +11,48 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro (
 
 
 // 1. chiedo all'utente di scegliere il livello di difficoltà : 1, 2 o 3 => let levelChoice = prompt ()
-    // 1.1 se l'utente sceglie livello 1 , genero una griglia di numeri con range 1-100/ griglia 10 x 10 => for
-    // 1.2 se l'utente sceglie livello 2 , genero una griglia di numeri con range 1-81/ griglia 9 x 9 => for
-    // 1.3 se l'utente sceglie livello 3 , genero una griglia di numeri con range 1-49/ griglia 7 x 7 => for
+    // 1.1 se l'utente sceglie livello 1 , genero una griglia di numeri con range 1-100/ griglia 10 x 10 
+    // 1.2 se l'utente sceglie livello 2 , genero una griglia di numeri con range 1-81/ griglia 9 x 9 
+    // 1.3 se l'utente sceglie livello 3 , genero una griglia di numeri con range 1-49/ griglia 7 x 7
 
 // 3. creo funzione di callback per cambiare il colore della cella cliccata 
 
 
-const gridContainer = document.getElementById("grid");
+let gridContainer = document.getElementById("grid");
+
 
 // chiedo all'utente di scegliere il livello di difficoltà : 1, 2 o 3 => let levelChoice = prompt ()
-let levelChoice = parseInt(prompt ("Da quale livello vuoi partire: 1, 2 o 3 ?"));
+let levelChoice = parseInt(prompt ("Da quale livello vuoi partire: 1-facile, 2-intermedio o 3-difficile ?"));
 console.log(levelChoice);
 
 
 if(levelChoice === 1) {
     // griglia 10 x 10
-    numeroBlocchi = 100;
-    baseClass = "square-l1";
+    generateGrid(100, "square-l1" );
 }else if(levelChoice === 2) {
     // griglia 9 x 9 
-    numeroBlocchi = 81;
-    baseClass = "square-l2";
+    generateGrid(81, "square-l2" );
 } else if(levelChoice === 3) {
     // griglia 7 x 7 
-    numeroBlocchi = 49;
-    baseClass = "square-l3";
+    generateGrid(49, "square-l3" );
 }
 
 
-for (let i = 1; i <= numeroBlocchi; i++) {
-    let node = document.createElement("div");
-    node.classList.add("square", baseClass);
-
-    node.addEventListener("click", 
-        function(){
-            node.classList.add("clicked-true")
-            node.append(i);
-        }
-    );
+function generateGrid(blocksNumber, baseClass) {
+    for (let i = 1; i <= blocksNumber; i++) {
+        let node = document.createElement("div");
+        node.classList.add("square", baseClass);
     
-    gridContainer.append(node);
-} 
+        node.addEventListener("click", 
+            function(){
+                node.classList.add("clicked-true");
+                node.append(i);
+            }
+        );
+    
+        gridContainer.append(node);
+    } 
+}
 
 
 
